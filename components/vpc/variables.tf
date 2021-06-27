350b0f9fa056fd712d4f6afe3bbd6aeb4509e734
KF <34534526+kf3225@users.noreply.github.com>
2021-06-27 12:26:48 +0900

    modify vpc (#2)
    Co-authored-by: kf3225

--- components/vpc/variables.tf
+++ components/vpc/variables.tf
@@ -4,8 +4,24 @@ variable "env_name" {
   description = "environment type"
 }
 
+variable "region" {
+  type        = string
+  default     = ""
+  description = "region"
+}
+
 variable "cidr_block" {
   type        = string
   default     = ""
-  description = "cidr block"
+  description = "cidr block of vpc"
+}
+
+variable "subnet_settings" {
+  type = list(object({
+    azs        = list(string)
+    cidr_block = list(string)
+    name       = string
+  }))
+  default     = []
+  description = "subnet settings list"
 }
--- modules/vpc.tf
+++ modules/vpc.tf
@@ -1,9 +1,13 @@
 variable "env_name" {}
+variable "region" {}
 variable "cidr_block" {}
+variable "subnet_settings" {}
 
 module "vpc" {
   source = "../../components/vpc"
 
-  env_name   = var.env_name
-  cidr_block = var.cidr_block
+  env_name        = var.env_name
+  region          = var.region
+  cidr_block      = var.cidr_block
+  subnet_settings = var.subnet_settings
 }
