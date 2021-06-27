bb8110a73a3062ecd15855522e1467f742a1b10f
kf3225 <clrflg.k.f@ezweb.ne.jp>
2021-06-27 12:25:01 +0900

    modify vpc

--- components/vpc/main.tf
+++ components/vpc/main.tf
@@ -1,3 +1,19 @@
+locals {
+  subnets = flatten([
+    for subnet in var.subnet_settings : [
+      for i in range(length(subnet.azs)) : {
+        cidr_block = subnet.cidr_block[i]
+        azs        = subnet.azs[i]
+        name       = subnet.name
+      }
+    ]
+  ])
+}
+
+#################################################
+# VPC
+#################################################
+
 resource "aws_vpc" "vpc" {
   cidr_block = var.cidr_block
 
@@ -6,16 +22,14 @@ resource "aws_vpc" "vpc" {
   }
 }
 
-resource "aws_subnet" "multi_subnet" {
+resource "aws_subnet" "subnet" {
   vpc_id = aws_vpc.vpc.id
 
-  count      = 4
-  cidr_block = cidrsubnet(aws_vpc.vpc.cidr_block, 8, count.index)
+  count             = length(local.subnets)
+  availability_zone = local.subnets[count.index].azs
+  cidr_block        = local.subnets[count.index].cidr_block
 
   tags = {
-    "Name" = (count.index % 2 == 0 ?
-      "wordpress-${var.env_name}-public-subnet-${count.index}" :
-      "wordpress-${var.env_name}-private-subnet-${count.index}"
-    )
+    "Name" = "wordpress-${var.env_name}-${local.subnets[count.index].name}-subnet-${count.index + 1}"
   }
 }
--- environments/dev/input.tfvars
+++ environments/dev/input.tfvars
@@ -1,2 +1,17 @@
 env_name = "dev"
+region   = "ap-northeast-1"
+
 cidr_block = "10.0.0.0/16"
+
+subnet_settings = [
+  {
+    azs        = ["ap-northeast-1a", "ap-northeast-1c"]
+    cidr_block = ["10.0.0.0/24", "10.0.2.0/24"]
+    name       = "public_subnet"
+  },
+  {
+    azs        = ["ap-northeast-1a", "ap-northeast-1c"]
+    cidr_block = ["10.0.1.0/24", "10.0.3.0/24"]
+    name       = "private_subnet"
+  }
+]
